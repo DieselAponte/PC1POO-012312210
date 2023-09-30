428b59bfb617a9e8bc2ad1bd7c954ada7ad82317
DieselAponte <143888820+DieselAponte@users.noreply.github.com>
2023-09-30 09:53:59 -0500

    Update PC1POO012312210.java

--- PC1POO012312210.java
+++ PC1POO012312210.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package pc1.poo012312210;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Alumno
- */
+
 public class PC1POO012312210 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         int puntosOb = 0;
         Scanner scanner = new Scanner(System.in);
